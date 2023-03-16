70494551003184d2a402764fa8ae3c3139a80e40
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-03-16 13:24:28 +0200

    error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,25 +10,27 @@ import {
 
 export const App = () => {
   return (
+
     // https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        React homework template test
+      </div>
       <ImageGallery />
       <ImageGalleryItem />
       <Loader />
       <Modal />
       <Searchbar />
       <Button />
-
-      React homework template test
-    </div>
+    </>
   );
 };
